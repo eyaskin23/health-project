d50e2a5aa123709ae634d72dde29d6df6639142e
eyaskin23 <evelynyaskin@gmail.com>
2024-08-02 15:35:48 -0400

    adding static_site

--- generate_static.py
+++ generate_static.py
@@ -3,29 +3,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Dummy data for demonstration purposes
-data = {
-    "fresno_aqi": 75,
-    "california_population": 38866193
-}
-
 @app.route('/')
 def index():
-    return render_template('index.html', data=data)
+    return render_template('index.html')
 
 @app.route('/social_determinants')
 def social_determinants():
+    data = {
+        'fresno_aqi': 75,
+        'california_population': 38866193
+    }
     return render_template('social_determinants.html', data=data)
 
 def build_static_site():
     with app.app_context():
+        # Define data for social determinants page
+        data = {
+            'fresno_aqi': 75,
+            'california_population': 38866193
+        }
+        
         templates = ['index.html', 'social_determinants.html']
         for template in templates:
-            rendered = render_template(template, data=data)
+            if template == 'social_determinants.html':
+                rendered = render_template(template, data=data)
+            else:
+                rendered = render_template(template)
+            
             output_path = os.path.join('static_site', template)
             os.makedirs(os.path.dirname(output_path), exist_ok=True)
             with open(output_path, 'w') as f:
                 f.write(rendered)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     build_static_site()
